989e1c8deea8c144cc7b7a2216be19efca341c47
zevlo <104938351+zevlo@users.noreply.github.com>
2025-10-30 21:16:37 -0400

    Fix backup script to save archive in destination dir

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,9 +5,9 @@ TIMESTAMP=$(date '+%Y-%m-%d_%H:%M')
 DEST_DIR="$HOME/backups"
 SOURCE_DIR="$HOME"
 
-# Create the final backup directory if it doesn't exist
 mkdir -p "$DEST_DIR"
 
-# Create a backup directly in the final
-tar -czvf "$SOURCE_DIR/home_backup_$TIMESTAMP.tar.gz" -C "$DEST_DIR" .
+# Save backup in DEST_DIR, and archive from SOURCE_DIR
+tar -czvf "$DEST_DIR/home_backup_$TIMESTAMP.tar.gz" -C "$SOURCE_DIR" .
+
 echo -e "\nBackup of $SOURCE_DIR completed at $DEST_DIR/home_backup_$TIMESTAMP.tar.gz\n"
