679c3e8df259f1ed79e9254a914ed67f14a2f2a9
zevlo <104938351+zevlo@users.noreply.github.com>
2025-11-02 11:07:51 -0500

    Delete service_monitor directory

--- service_monitor/service_monitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SERVICE="nginx"
-if ! pgrep -x "$SERVICE" > /dev/null; then
-  echo "$SERVICE is down. Restarting..."
-  systemctl start $SERVICE
-else
-  echo "$SERVICE is running."
-fi
